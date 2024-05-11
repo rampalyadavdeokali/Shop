352e8a5243cdfc5133b3c0e59578c58f0516b95f
Rampal Yadav <133230206+rampalyadavdeokali@users.noreply.github.com>
2024-05-11 19:00:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+ function test(){
+   var temp = document.getElementById('h1');
+   temp.style.color = "red";
+   temp.style.fontSize = "200px";
+   temp.style.fontWeight = "bold";
+   temp.style.textDecoration = "underline";
+   temp.style.border = "1px solid blue";
+   temp.style.borderRadius = "10px";
+   temp.style.backgroundColor = "yellow";
+ }
+
+
+ function para(){
+    var temp = document.getElementById('para');
+    temp.style.color = "red";
+    temp.style.fontSize = "200px";
+    temp.style.fontWeight = "bold";
+    temp.style.textDecoration = "underline";
+    temp.style.border = "1px solid blue";
+    temp.style.borderRadius = "10px";
+    temp.style.backgroundColor = "yellow";
+ }
